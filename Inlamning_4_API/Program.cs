4b582a17f940471e2e2daa6fc0f768d592926c2e
Sebastian Lindén <sebastian.linden@learnet.se>
2024-12-01 13:39:24 +0100

    Add project files.

---
+++ Inlamning_4_API/Program.cs
@@ -0,0 +1,28 @@
+﻿using Inlamning_4_API;
+using System.Text.Json;
+
+
+using HttpClient client = new()
+{
+    BaseAddress = new Uri("https://api.github.com/orgs/dotnet/"),
+    DefaultRequestHeaders = { { "User-Agent", "ConsoleApp" } }
+};
+
+var repos = await GetPostsAsync(client);
+
+foreach (var r in repos)
+{
+    Console.WriteLine(r + "\n");
+}
+
+
+
+
+static async Task<List<Repo>> GetPostsAsync(HttpClient client)
+{
+
+    await using Stream stream = await client.GetStreamAsync("repos");
+    var result = await JsonSerializer.DeserializeAsync<List<Repo>>(stream);
+
+    return result ?? new();
+}
---
+++ Inlamning_4_API/Repo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Inlamning_4_API
+{
+    internal class Repo
+    {
+        [property: JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [property: JsonPropertyName("homepage")]
+        public string Homepage { get; set; }
+
+        [property: JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [property: JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [property: JsonPropertyName("watchers_count")]
+        public int Watchers { get; set; }
+        
+        [property: JsonPropertyName("pushed_at")]
+        public DateTime PushedAt { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{this.Name} \n Homepage:{this.Homepage} \n GitHub:{this.HtmlUrl} \n Description:{this.Description} \n Watchers:{this.Watchers} \n Latest Push:{this.PushedAt}";
+        }
+
+    }
+}
